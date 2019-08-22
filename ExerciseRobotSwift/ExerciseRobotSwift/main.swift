dc7d34b1975deec37e08e420e1ca7818be4fcd59
Goglow <48105051+Goglow@users.noreply.github.com>
2019-08-22 08:48:26 -0400

    Update main.swift

--- ExerciseRobotSwift/ExerciseRobotSwift/main.swift
+++ ExerciseRobotSwift/ExerciseRobotSwift/main.swift
@@ -8,10 +8,13 @@
 
 import Foundation
 
+// 1 - Create a robot class and generate a robot (generate a robot)
 var oneRobot = Robot(robotName: "Roby")
 
+// 2 - Robot has a friend (generate a second robot)
 var twoRobot = Robot(robotName: "Margotte")
 
+// 4 - An army of robots (make robots to introduce)
 var threeRobot = Robot(robotName: "Sunny", pointsOfLife: 90, maximumSpeedOfMovement: 2, position: (0, 1))
 
 var fourRobot = Robot(robotName: "Wayo")
@@ -19,15 +22,18 @@ var fourRobot = Robot(robotName: "Wayo")
 var listOfRobots = [oneRobot, twoRobot, threeRobot, fourRobot]
 
 for robots in listOfRobots {
-    robots.introdiuce()
+    robots.introduce()
 }
 
-var fiveRobot = Robot(robotName: "Dadou")
+var fiveRobot = Robot(robotName: "Dino")
 fiveRobot.move(direction: .up, choiceSpeed: 2)
-fiveRobot.introdiuce()
+fiveRobot.introduce()
 
 var badRobot = Bad(damageMelee: 20, damageRemote: true)
 
-badRobot.introdiuce()
-badRobot.attack(robots: twoRobot)
-twoRobot.introdiuce()
+badRobot.introduce()
+badRobot.attack(target: twoRobot)
+twoRobot.introduce()
+fourRobot.seDeplacerAleatoirement(coups: 5)
+fourRobot.introduce()
+

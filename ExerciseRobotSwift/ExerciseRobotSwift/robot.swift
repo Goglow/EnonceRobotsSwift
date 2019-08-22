8109043e46130ad18d4f6f4041c68a5f01dd1c4a
Goglow <48105051+Goglow@users.noreply.github.com>
2019-08-22 08:45:31 -0400

    Update robot.swift

--- ExerciseRobotSwift/ExerciseRobotSwift/robot.swift
+++ ExerciseRobotSwift/ExerciseRobotSwift/robot.swift
@@ -8,7 +8,8 @@
 
 import Foundation
 
-public class Robot {
+// 1 - Create a robot class and generate a robot (create a robot class)
+class Robot {
     var robotName: String
     var pointsOfLife: Int
     var maximumSpeedOfMovement: Int
@@ -20,37 +21,43 @@ public class Robot {
         maximumSpeedOfMovement = 3
         position = (0, 0)
     }
-    
+// 7 - Unique robots
     init(robotName: String, pointsOfLife: Int, maximumSpeedOfMovement: Int, position: (Int, Int)) {
         self.robotName = robotName
         self.pointsOfLife = pointsOfLife
         self.maximumSpeedOfMovement = maximumSpeedOfMovement
         self.position = position
     }
-    
-    func introdiuce() {
-        print("Bonjour je m'appelle \(robotName). J'ai \(pointsOfLife) points de vie et je me déplace à \(maximumSpeedOfMovement) cases par seconde. Je suis à la case de coordonnées \(position).")
+// 3 - Talking robots (ask each robot to introduce themselves)
+    func introduce() {
+        print("Bonjour je m'appelle \(self.robotName). J'ai \(self.pointsOfLife) points de vie et je me déplace à \(self.maximumSpeedOfMovement) cases par seconde. Je suis à la position X: \(self.position.X), Y: \(self.position.Y).")
     }
-    
+// 5 - Robots conquer the world
     func move(direction: Direction, choiceSpeed: Int) {
+        var speed = 0
         if choiceSpeed <= 3 {
-            maximumSpeedOfMovement = choiceSpeed
+            speed =  choiceSpeed
         } else {
-            maximumSpeedOfMovement = 3
+            speed =  maximumSpeedOfMovement
         }
         switch direction {
         case .up :
-            self.position.Y += choiceSpeed
+            self.position.Y += speed
         case .down :
-            self.position.Y -= choiceSpeed
+            self.position.Y -= speed
         case .left :
-            self.position.X -= choiceSpeed
+            self.position.X -= speed
         case .right :
-            self.position.X += choiceSpeed
+            self.position.X += speed
         }
+        print("\(self.robotName) est maintenant a la position X: \(self.position.X), Y: \(self.position.Y)")
     }
-    
-    func seDeplacerAleatoirement(coups: Int) -> Int {
-        return Int(arc4random_uniform(UInt32(coups)))
+// 6 - In Brownian motion
+    func seDeplacerAleatoirement(coups: Int) {
+        for _ in 1...coups {
+            let speed = Int.random(in: 0...maximumSpeedOfMovement)
+            let direction = Direction.allCases.randomElement()!
+            move(direction: direction, choiceSpeed: speed)
+        }
     }
 }
